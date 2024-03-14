956015de8f8828948d30c307a644b155af48f8dc
lufra <lukas@audriga.com>
2024-03-14 11:30:00 +0100

    removed Card Object, created dedicated subtemplates map, removed almost all find functions, removed unwanted linebreaks from some subtemplates

--- jsonld2html.js
+++ jsonld2html.js
@@ -8,95 +8,43 @@ import extractImage from './lib/image_extraction.js';
 import createPotentialViewAction from './lib/view_action.js'
 import typeToIconMap from './lib/type_to_icon_map.js';
 import {getDefaultCardSubtemplate, hasDefaultCardSubtemplate} from './lib/template_exporter.js';
-
-
-
-/**
- * Data Object used as transfer between data_object from jsonld file and the mustache template
- */
-function Card(_pictureURL, _iconName="image",_title, _content = [] ,
-     _footer, _breadcrumbList, _dedicatedTextColumn, _urlAction,_copyAction){
-    this.pictureURL = _pictureURL;
-    this.iconName = _iconName;
-    this.title = _title;
-    this.content = _content;
-    this.dedicatedTextColumn = _dedicatedTextColumn;
-    this.footer = _footer;
-    this.breadcrumbList = _breadcrumbList;
-    this.urlAction = _urlAction;
-    this.copyAction = _copyAction;
-    
-}
+import {hasDedicatedSubtemplate, getDedicatedSubtemplate} from './lib/template_exporter.js';
 
 var jsonld2html = {
     name: 'jsonld2html.js',
     version: '0.0.1'
 }
 
-
-/**
- * @param {object} entireObj - Object to search
- * @þaram {string} keyToFind - the Key to search for a value
- * @return {string} if a key exists the value of the key will returned
- */
-function findValueFromKey(entireObj, keyToFind) {
-    let foundValue;
-
-    for (let keysKey of Object.keys(entireObj)) {
-        if (keysKey !== keyToFind) {
-            continue;
+        /** 
+* Find values for a specified key in an array of json objects
+* @param object object - the object to be searched
+* @return object Returns the value matching to the provided key
+*/
+function findValueInArray(object,key){
+    // case object is array
+    if(Array.isArray(object)){
+        for (const iterator of object) {
+            if(key in iterator){
+                return iterator[key];
+            }
         }
-        foundValue = (entireObj[keysKey]);
     }
-    return foundValue;
 }
-
-/**
- * finds Key Values in deep nests and returns the object, depth is not restricted !
- */
-function findNestedObj(entireObj, keyToFind) {
-    let foundObj;
-    JSON.stringify(entireObj, (_, nestedValue) => {
-        if (nestedValue && nestedValue[keyToFind]){
-            foundObj = nestedValue;
-        }
-        return nestedValue;
-    });
-    if(foundObj != undefined) {return foundObj;}
-    else return null;
-}
-
-/**
- * this function is used to find objects regardless of their location in the object
- * it returns the object in which to key and val matching
- */
-function findNestedObjWithValue(entireObj, keyToFind, valToFind) {
-    let foundObj;
-    JSON.stringify(entireObj, (_, nestedValue) => {
-        if (nestedValue && nestedValue[keyToFind] === valToFind) {
-            foundObj = nestedValue;
-        }
-        return nestedValue;
-    });
-    return foundObj;
-}
-
+// TODO Create docstring
 function renderFromTemplate(jsonLd, template, dedicatedType = "") {
-
-
 
     if(typeToIconMap.has(jsonLd["@type"])) {
         jsonLd["iconName"] = typeToIconMap.get(jsonLd["@type"]);
     }
-    // in case we dont have a type specific icon we use a default icon
+   
+    // in case we dont have a schema type specific icon we use a default icon
     else { jsonLd["iconName"] = "image";}
     
-
     // ===== Special case "FlightReservationArray" =====
     if(dedicatedType === "FlightReservationArray"){
-
         // Creating ID for the bar to wire it with its tabs
         let tab_bar_id = "bar" + Math.floor(Math.random() * 100);
+
         // adding an synthetic ID to the instances
         let i = Math.floor(Math.random() * 1000000000000001);
 
@@ -112,48 +60,31 @@ function renderFromTemplate(jsonLd, template, dedicatedType = "") {
     }
     
     // ===== Special case "PromotionCards" =====
-    if(dedicatedType === "PromotionCards")
+    if( dedicatedType === "PromotionCards" &&
+        hasDedicatedSubtemplate("PromotionCards"))
     {
-        function findNestedObjectsWithVal(entireObj, keyToFind, valToFind) {
-            let foundObj= [];
-            JSON.stringify(entireObj, (_, nestedValue) => {
-                if (nestedValue && nestedValue[keyToFind] === valToFind)  {
-                    foundObj.push(nestedValue);
-                }
-                return nestedValue;
-            });
-            if(foundObj.length >= 1){
-                return foundObj;
-            }
-            else return null;
-        }
-        
-        function findNestedObjWithKey(entireObj, keyToFind) {
-            let foundObj;
-            JSON.stringify(entireObj, (_, nestedValue) => {
-                if (nestedValue && nestedValue[keyToFind]){
-                    foundObj = (nestedValue[keyToFind]);
-                }
-                return nestedValue;
-            });
-            return foundObj;
-        }
-        
+    
         function getCurrencyChar(_symbol){
             if(_symbol === "USD"){
                 return "$";
             }
             else {return _symbol;}
         }
-        
+
         let mustacheDataObj = new Object();
         mustacheDataObj["promotionCards"] = [];
-        mustacheDataObj["logo"] = findNestedObjWithKey(jsonLd,"logo");
-        mustacheDataObj["subjectLine"] = findNestedObjWithKey(jsonLd,"subjectLine");
-        mustacheDataObj["description"] = findNestedObjWithKey(jsonLd,"description");
-        mustacheDataObj["discountCode"] = findNestedObjWithKey(jsonLd,"discountCode");
+        mustacheDataObj["logo"] = findValueInArray(jsonLd,"logo");
+        mustacheDataObj["subjectLine"] = findValueInArray(jsonLd,"subjectLine");
+        mustacheDataObj["description"] = findValueInArray(jsonLd,"description");
+        mustacheDataObj["discountCode"] = findValueInArray(jsonLd,"discountCode");
         
-        let foundObjects= findNestedObjectsWithVal(jsonLd,"@type","PromotionCard");
+        let foundObjects = [];
+
+        for (const iterator of jsonLd) {
+            if(iterator["@type"] === "PromotionCard"){
+                foundObjects.push(iterator);
+            }
+        }
         
         for (const obj of foundObjects){
             
@@ -161,12 +92,11 @@ function renderFromTemplate(jsonLd, template, dedicatedType = "") {
                 image: obj["image"],
                 headline: obj["headline"],
                 discountValue: obj["discountValue"],
-                newPrice: getCurrencyChar(obj["priceCurrency"]) + String(obj["price"] - obj["discountValue"]),
-                oldPrice: getCurrencyChar(obj["priceCurrency"]) + String(obj["price"]),
+                newPrice: String(obj["price"] - obj["discountValue"]),
+                oldPrice: String(obj["price"]),
                 priceCurrency: obj["priceCurrency"]
             }
-            mustacheDataObj["promotionCards"].push(mustache.render(getDefaultCardSubtemplate(dedicatedType),promoCardTest))
-            
+            mustacheDataObj["promotionCards"].push(mustache.render(getDedicatedSubtemplate(dedicatedType),promoCardTest))
         }
         
         let finalCard = mustache.render(template, mustacheDataObj);
@@ -235,7 +165,7 @@ jsonld2html.render = function render(jsonLd) {
     // Preprocessing
     let preprocessedJson = extractImage(createPotentialViewAction(getMainEntity(jsonLd)));
     // TODO - replace "Find" function
-    return renderFromTemplate(preprocessedJson, getTemplate(findValueFromKey(preprocessedJson,"@type")));
+    return renderFromTemplate(preprocessedJson, getTemplate(preprocessedJson["@type"]));
 }
 
 jsonld2html.renderFromTemplate = renderFromTemplate;
