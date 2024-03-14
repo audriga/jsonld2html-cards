6e3d11f2174eacf379446355aa0915ac788c1730
lufra <lukas@audriga.com>
2024-03-14 15:22:03 +0100

    more icons

--- jsonld2html.js
+++ jsonld2html.js
@@ -46,7 +46,7 @@ function renderFromTemplate(jsonLd, template, artificialType = "") {
     }
    
     // in case we dont have a schema type specific icon we use a default icon
-    else { jsonLd["iconName"] = "image";}
+    else { jsonLd["iconName"] = "question";}
     
     // ===== Special case "FlightReservationArray" =====
     if(artificialType === "artificial_FlightReservationArray"){
